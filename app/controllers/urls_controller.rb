87403140844b3423ed9531353c611442fcedaa0b
Hari <hariprasad@pharmeasy.in>
2017-08-21 11:25:08 +0530

    url shortner api set

--- app/controllers/urls_controller.rb
+++ app/controllers/urls_controller.rb
@@ -2,6 +2,7 @@ class UrlsController < ApplicationController
   #before_action :set_url, only: [:show, :edit, :update, :destroy]
   before_action :find_url, only: [:show]
 
+  $chars = ['0'..'9', 'A'..'Z', 'a'..'z'].map { |range| range.to_a }.flatten
   # GET /urls
   # GET /urls.json
   def index
@@ -24,10 +25,9 @@ class UrlsController < ApplicationController
   end
 
   def generate_base62_short_url(short_url_generator_id)
-    @chars = ['0'..'9', 'A'..'Z', 'a'..'z'].map { |range| range.to_a }.flatten
     short_url = ""
     while short_url_generator_id > 0 do
-      short_url << @chars[short_url_generator_id % 62]
+      short_url << $chars[short_url_generator_id % 62]
       short_url_generator_id = short_url_generator_id / 62
     end
     short_url
@@ -87,14 +87,13 @@ class UrlsController < ApplicationController
   def find_url
     #get the id from slug
     count = 0
-    params[:id].reverse.split("").each { |x| count = count * 62 + @chars.index(x)  }
+    params[:id].reverse.split("").each { |x| count = count * 62 + $chars.index(x)  }
     @url = Url.find_by_short_url(params[:id])
   end
 
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_url
-
       @url = Url.find(params[:id])
     end
 
