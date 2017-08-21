ec472b22c5660fe9244ed9aee4aedff46c1d32f5
Hari <hariprasad@pharmeasy.in>
2017-08-21 13:28:52 +0530

    cros filter added

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,11 @@
 class ApplicationController < ActionController::Base
+  before_action :cors_filter
 
+  def cors_filter
+    headers['Access-Control-Allow-Origin'] = request.headers['Origin']
+    headers['Access-Control-Allow-Methods'] = 'GET, POST, PUT, PATCH, DELETE, OPTIONS'
+    headers['Access-Control-Allow-Headers'] = 'Origin, Content-Type, Authorization, Accept, X-Requested-With'
+    headers['Access-Control-Max-Age'] = '1728000'
+    headers['Access-Control-Allow-Credentials'] = true
+  end
 end
